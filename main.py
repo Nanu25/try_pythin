a4a6e030cb733475bb6ff8b215436aeba7fb5c21
Grancea Alexandru <alexandrugrancea25@gmail.com>
2023-10-10 09:26:48 +0300

    test

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Merge")
+"""
+This will be a password generator with different simbol, letter, and numbers
+"""
+
+print("Tell what's the lenght of the password. The minimum lenght is 6 ")
+n = int(input(">"))
